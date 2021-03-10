3f6d76921ba0bea109e71939a88d30e26bb9ba63
Chris Stead <cmstead@gmail.com>
2021-03-10 10:15:25 -0800

    Updated gitignore, added file loaders

--- PyAutoDI/PyAutoDI.py
+++ PyAutoDI/PyAutoDI.py
@@ -1,4 +1,23 @@
+from importlib import import_module
+from glob import glob
+
 from .Container import Container
+
+def get_module_paths(path_pattern):
+    return glob(path_pattern)
+
+def load_modules(module_paths):
+    return map(import_module, module_paths)
+
+def register_module(module, container):
+    module.register(container)
+
+def load_and_register_modules(path_pattern, container):
+    module_paths = get_module_paths(path_pattern)
+    modules = load_modules(module_paths)
+    
+    for module in modules:
+        register_module(module, container)
 
 def get_new_container():
     return Container()
--- setup.py
+++ setup.py
@@ -1,9 +1,25 @@
-from distutils.core import setup
+import pathlib
+from setuptools import setup, find_packages
+
+CURRENT_DIR = pathlib.Path(__file__).parent
+README_CONTENT = (CURRENT_DIR / "README.md").read_text()
 
 setup(
-    name='PyAutoDI',
+    name='py-auto-di',
     version='0.1.0',
-    packages=['PyAutoDI'],
+    packages=["PyAutoDI"],
+    include_package_data=True,
+    author="Chris Stead",
+    author_email="cmstead@gmail.com",
+    url="https://github.com/cmstead/py-auto-di#readme",
+    platforms=['any'],
+    description="Lightweight, automatic Python DI library",
     license='MIT',
-    long_description=open('README.md').read(),
+    long_description=README_CONTENT,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+    ],
 )
